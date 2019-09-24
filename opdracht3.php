e81674ea209b93296f67dfda958b162b8d2b8cea
SchoolAccB <bbrands@roc-dev.com>
2019-09-24 10:48:08 +0200

    Opdracht 3

--- opdracht3.php
+++ opdracht3.php
@@ -5,5 +5,6 @@ $boodschappen = ["aardappelen", "aardbeien", "3 pakken melk", "yoghurt"];
 echo "<ul>";
 foreach($boodschappen as $boodschap) {
     echo "<li>".$boodschap."</li>";
+    echo "<img src='Resources/".$boodschap.".jpg'>";
 }
 echo "</ul>";
